53852e2f12294f334bf16977ae794fee43c8bb6d
vinith03 <vinithmarsh@gmail.com>
2016-06-05 22:25:56 +0530

    Create Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Conversion
+{
+public static void main(String a[])
+{
+Scanner ip=new Scanner(System.in);
+String str=ip.next();
+int num=Integer.parseInt(str);
+System.out.println(num);
+}
+}
